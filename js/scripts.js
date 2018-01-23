8f244c95232e5d53a56e87e129990764965849dd
Will Worthington <willworthington99@yahoo.com>
2018-01-23 16:38:30 -0600

    Assignment 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
-(document).ready(function(){
-    ("#mycarousel").carousel({interval: 2000});
-    ("#carousel-button").click(function(){
-        if(("#carousel-button").children("span").hasClass("fa-pause")){
-            ("#mycarousel").carousel('pause');
-            ("#carousel-button").children("span").removeClass("fa-pause");
-            ("#carousel-button").children("span").addClass("fa-play");
+$(document).ready(function(){
+    $("#mycarousel").carousel({interval: 2000});
+    $("#carousel-button").click(function(){
+        if($("#carousel-button").children("span").hasClass("fa-pause")){
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass("fa-pause");
+            $("#carousel-button").children("span").addClass("fa-play");
         }
-        else if (("#carousel-button").children("span").hasClass("fa-play")){
-            ("#mycarousel").carousel('cycle');
-            ("#carousel-button").children("span").removeClass("fa-play");
-            ("#carousel-button").children("span").addClass("fa-pause");
+        else if ($("#carousel-button").children("span").hasClass("fa-play")){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass("fa-play");
+            $("#carousel-button").children("span").addClass("fa-pause");
         }
     });
+});
+
+$(document).ready(function(){
+   $("#reserveButton").click(function(){
+       $("#reserveModal").modal('show');
+   })
+});
+
+$(document).ready(function(){
+   $("#loginButton").click(function(){
+       $("#loginModal").modal('show');
+   })
 });
